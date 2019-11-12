0b41e386539d291223809b55c5db79da0f23abf3
Daniel Correa <danielfernandocorrea1@gmail.com>
2019-11-12 18:39:51 -0500

    end, class 6

--- external/webpack.config.js
+++ external/webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     entry: path.resolve(__dirname, 'index.js'),
-    mode: 'production',
+    mode: 'development',
     output: {
-        path: path.resolve(__dirname),
+        path: path.resolve(__dirname, 'dist'),
         filename: './bundle.js',
     }
 }
